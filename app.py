ae9d8ab712686d5068c73a17ecb1334ec5388188
Your Name <you@example.com>
2026-03-14 14:01:48 -0500

    fixing typo

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-import flask
+from flask import flask
+import time
+app = Flask(__name__)
+@app.route('/')
+
+def get_current_time():
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    return f'The current time is: {current_time}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
